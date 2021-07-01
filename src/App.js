ee6fd218800af85c1cc78380e7f7390e1a58ab2e
Hrithikrana377 <hrithikranaji@gmail.com>
2021-07-01 12:33:00 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button, Grid, Typography } from '@material-ui/core';
+import Navbar from './components/Navbar';
+import { getMatches } from "./api/Api";
+import { Fragment, useEffect, useState } from 'react';
+import MyCard from './components/MyCard';
 function App() {
+  const [matches,setMatches] = useState([]);
+
+  useEffect(() => {
+    getMatches()
+      .then((data) => {
+        console.log(data);
+        setMatches(data.matches);
+      })
+      .catch((error) => {});
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Typography variant="h3">Welcome to my app</Typography>
+      <Grid container>
+        <Grid sm="2"></Grid>
+        <Grid sm="8" >
+          {matches.map((match)=>(
+            <Fragment key={match.unique_id}>
+            {match.type == "Twenty20" ? (<MyCard key={match.unique_id} match={match} />) : ("")}
+            </Fragment>
+          ))} 
+        </Grid>
+      </Grid> 
+      
     </div>
   );
 }
